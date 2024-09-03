ee4969d08700ea4fb8da321053b8907719084b23
Sachin Rajput <165654923+sachinrajput9810@users.noreply.github.com>
2024-09-03 15:48:35 +0530

    Added Authorization and Authentication in express

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const userModel = require("./models/user.js")
+const bcrypt = require('bcrypt')
+
+
+const app = express() 
+app.set("view engine" , "ejs") ;
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname , 'public' )))
+
+
+app.get("/" , (req , res) => {
+    res.render("index")
+})
+
+app.post("/create" , (req,res) =>{
+
+    let {username , email , password , age} = req.body 
+
+    bcrypt.genSalt(10 , (err , salt) => {
+        bcrypt.hash(password , salt , async (err , hash) => {
+
+            let createdUser = await userModel.create({
+                username ,
+                email , 
+                password  : hash ,
+                age 
+            })
+
+            let token = jwt.sign({email} , "secret_key")
+            res.cookie("token" , token)
+            res.send(createdUser) 
+        })
+    })
+}) 
+
+app.get("/login" , (req , res) => {
+    res.render("login")
+})
+
+app.post("/login" , async (req , res) => {
+    let user = await userModel.findOne({email : req.body.email}) ;
+    if(!user) res.send("Something went wrong")
+        
+    bcrypt.compare(req.body.password , user.password , (err , result)=> {
+        if(result){
+            let token = jwt.sign({email : user.email} , "secret_key")
+            res.cookie("token" , token)
+            res.send("Login successfully")
+        }
+        else res.send("Something went wrong")
+    })
+})
+
+
+app.get("/logout" , (req , res) => {
+    res.cookie("token" , "")
+    res.redirect("/")
+})
+
+
+
+
+
+
+app.listen(3000)
+
+
+
+
+
+
+
